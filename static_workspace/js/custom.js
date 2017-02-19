55d472cf61363ca099672e108c6d2e405ac0dd58
Henrique Macedo <henriquemacedo@me.com>
2017-02-19 10:32:50 +0000

    Add swipe for next and previous entrie

--- static_workspace/js/custom.js
+++ static_workspace/js/custom.js
@@ -10,3 +10,43 @@ function getUrlVars()
     }
     return vars;
 }
+
+// SWIPE
+$(function() {
+  $(".news-container__entrie").swipe({
+    swipeStatus:function(event, phase, direction, distance) {
+      var str = phase;
+      str += distance;
+
+      if(phase=="start") {
+        $(".news-container__entrie").addClass("moving");
+      }
+
+      if(phase=="move" && direction=="left") {
+        $(".news-container__entrie").css("margin-left", "-"+distance+"px");
+
+        if (distance>100)
+          window.location.href = "#next-entrie";
+      }
+
+      if(phase=="move" && direction=="right") {
+        $(".news-container__entrie").css("margin-left", distance+"px");
+
+        if (distance>100)
+          window.location.href = "#previous-entrie";
+      }
+
+      if(phase=="end") {
+        $(".news-container__entrie").removeClass("moving");
+        $(".news-container__entrie").css("margin-left", "0");
+      }
+
+      if(phase=="cancel") {
+        $(".news-container__entrie").removeClass("moving");
+        $(".news-container__entrie").css("margin-left", "0");
+      }
+
+      // $(".news-container__entrie").html(str);
+    }
+  });
+});
